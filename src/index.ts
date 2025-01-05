3ad951da23b96e53a1e8ebf216e4202de4829b2f
Langning Chen <chenlangning2009@outlook.com>
2025-01-05 13:10:14 +0800

    Update date

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**********************************************************************
 FileShare: Share files using Cloudflare Workers and GitHub Private Repositories!
-Copyright (C) 2024  langningchen
+Copyright (C) 2025  langningchen
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
